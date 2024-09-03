f618028ba62b94596b3c6932f56cd1d5bca5fae0
LeoTan2004 <3509836544@qq.com>
2024-09-03 09:45:43 +0800

    feat 完成登陆操作
    学生账户登陆测试成功。使用环境变量测试
    
    Signed-off-by: LeoTan2004 <3509836544@qq.com>

--- src/ems/ems.py
+++ src/ems/ems.py
@@ -1,8 +1,13 @@
 """教务系统模块"""
+import json
 from abc import ABC, abstractmethod
 
+import requests
+
 from ems.account import AuthenticationAccount
+from ems.config import XTUEMSConfig
 from ems.session import Session
+from util.captcha import ImageDetector
 
 
 class EducationalManageSystem(ABC):
@@ -30,10 +35,75 @@ class EducationalManageSystem(ABC):
 
 
 class ZQEducationalManageSystem(EducationalManageSystem):
-    """智强教务系统"""
+    """
+    智强教务系统
+    """
+    _SESSION_NAME = "JSESSIONID"
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.captcha = ImageDetector()
 
     def login(self, account: AuthenticationAccount) -> Session:
-        pass
+        with requests.session() as ems_session:
+            resp = ems_session.get(XTUEMSConfig.XTU_EMS_CAPTCHA_URL)
+            session_id = resp.cookies.get(ZQEducationalManageSystem._SESSION_NAME)
+            session = Session(session_id=session_id)
+            captcha = self.captcha.verify(resp.content)
+            resp = ems_session.post(XTUEMSConfig.XTU_EMS_SIG_URL)
+            signature = json.loads(resp.content).get("data")
+            encoded = self._signature(account.username, account.password, signature)
+            headers = {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+            resp = ems_session.post(XTUEMSConfig.XTU_EMS_LOGIN_URL, data={
+                "USERNAME": account.username,
+                "PASSWORD": account.password,
+                "encoded": encoded,
+                "RANDOMCODE": captcha,
+            }, headers=headers, allow_redirects=False)
+            if resp.status_code != 302:
+                raise Exception("登陆失败")
+        return session
+
+    def _signature(self, username: str, password: str, signature: str):
+        """
+        智强教务系统签名算法
+        Args:
+            username: 用户名
+            password: 密码
+            signature: 加密签名
+
+        Returns:
+            返回签名后的密码
+
+        """
+        # 将data按照'#'分割成列表
+        split = signature.split("#")
+        # 创建code字符串
+        code = username + "%%%" + password
+        # 初始化编码结果的StringBuilder（在Python中使用列表模拟）
+        encoded = []
+        # 获取code的长度
+        length = len(code)
+        # 初始化偏移量b
+        b = 0
+        # 遍历code中的每个字符
+        for i in range(length):
+            if i < 20:
+                # 追加code中的当前字符
+                encoded.append(code[i])
+                # 追加split[0]中从b开始的由split[1][i]指定数量的字符
+                for _ in range(ord(split[1][i]) - ord('0')):
+                    encoded.append(split[0][b])
+                    b += 1
+            else:
+                # 追加code从索引20开始的剩余部分
+                encoded.extend(code[20:])
+                break
+
+        # 将列表转换为字符串并返回
+        return ''.join(encoded)
 
     def get_courses(self, session: Session):
         pass
