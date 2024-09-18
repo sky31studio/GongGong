81680751e48acdad18564c7faa1a527aa6478588
LeoTan2004 <3509836544@qq.com>
2024-09-18 17:19:15 +0800

    chore 调整测试的目录结构
    Signed-off-by: LeoTan2004 <3509836544@qq.com>

--- tests/test_xtu_ems/test_captcha.py
+++ tests/test_xtu_ems/test_captcha.py
@@ -5,7 +5,7 @@ from xtu_ems.util.captcha import ImageDetector
 
 class TestImageDetector(TestCase):
     def test_verify(self):
-        with open('./verifycode.jpeg', 'rb') as f:
+        with open('verifycode.jpeg', 'rb') as f:
             img = f.read()
         res = ImageDetector().verify(img)
         self.assertEqual('cnbz', res)
