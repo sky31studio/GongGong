c463e24e6d4744526364e8e75e21c1aef5a889d8
LeoTan2004 <3509836544@qq.com>
2024-10-24 14:48:23 +0800

    fix(handler): 修复爬虫遇到空格字符的问题
    Signed-off-by: LeoTan2004 <3509836544@qq.com>

--- src/xtu_ems/ems/handler/get_student_exam.py
+++ src/xtu_ems/ems/handler/get_student_exam.py
@@ -21,10 +21,10 @@ class StudentExamGetter(EMSPoster[list[ExamInfo]]):
     def _extra_exam_info(self, row: BeautifulSoup) -> ExamInfo:
         """从表格的某一行中提取学生的考试信息"""
         tds = row.find_all('td')
-        return ExamInfo(name=tds[2].text,
-                        type=tds[3].text,
-                        time=tds[5].text,
-                        location=tds[6].text)
+        return ExamInfo(name=tds[2].text.strip(),
+                        type=tds[3].text.strip(),
+                        time=tds[5].text.strip(),
+                        location=tds[6].text.strip())
 
     def url(self):
         return XTUEMSConfig.XTU_EMS_STUDENT_EXAM_URL
