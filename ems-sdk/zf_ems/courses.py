3191f736ed9473c92edd3028f47860e8c9c5186f
LeoTan2004 <3509836544@qq.com>
2025-09-06 23:37:48 +0800

    refactor(courses): 重构课程获取逻辑，添加课程时间解析功能
    Signed-off-by: LeoTan2004 <3509836544@qq.com>

--- ems-sdk/zf_ems/courses.py
+++ ems-sdk/zf_ems/courses.py
@@ -1,9 +1,33 @@
 from common.exception import *
+from common.model import CourseList, CourseInfo
 from common.sess import HttpSessionHolder
 from zf_ems.config import *
 
 
-async def get_courses(session: HttpSessionHolder, year, term) -> list[dict]:
+def parse_course_time(courses_list) -> CourseList:
+    """
+    解析课程时间
+
+    :param courses_list: 原始课程列表
+    :return: 解析后的课程列表
+    """
+    courses = []
+    for course in courses_list:
+        course_info = CourseInfo()
+        course_info.name = course.get("kcmc", "")
+        course_info.teacher = course.get("xm", "")
+        course_info.classroom = course.get("cdmc", "")
+        course_info.weeks = course.get("zcd", "").replace("周", "")
+        jc = course.get("jc", "1-2节")
+        course_info.start_time = int(jc.split("-")[0])
+        course_info.duration = int(jc.split("-")[1].replace("节", "")) - course_info.start_time + 1
+        course_info.day = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"][
+            int(course.get("xqj", "1")) - 1]
+        courses.append(course_info)
+    return CourseList(courses=courses)
+
+
+async def get_courses(session: HttpSessionHolder, year, term) -> CourseList:
     """
     获取课程列表
 
@@ -25,4 +49,5 @@ async def get_courses(session: HttpSessionHolder, year, term) -> list[dict]:
         ) as response:
             if response.status != 200:
                 raise SessionInvalidException("Failed to fetch courses")
-            return await response.json()
+            result = await response.json()
+            return parse_course_time(result['kbList'])
